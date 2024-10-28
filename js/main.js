ee65d87e4ba5d067342b5959260ad225bca0b403
Leyla Muradzade <lvmuradzade55@gmail.com>
2024-10-28 15:35:54 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,125 @@
+//1
+// let num1 = 0 
+// if(num1 > 0){
+//     console.log("musbetdir");
+// }else if(num1 < 0){
+//     console.log("menfidir");
+// }else if(num1 == 0){
+//     console.log("beraberdir");  
+// }
+
+
+//2
+// let user = "admin"; 
+
+// switch (user) {
+// case "admin":
+// console.log("admin ile daxil oldunuz");
+//          break;
+//      case "istifadəçi":
+//          console.log("stifadeci girishi ile daxil oldunuz");
+//          break;
+//      case "qonaq":
+//          console.log("qonaq girishi ile daxil oldunuz");
+//          break;
+//      default:
+//          console.log("giris duzgun deyil");
+//  }
+
+
+//3
+// let num1 = prompt("reqem yazin")
+// if(num1 %2 == 0){
+//     console.log(num1 + "cut");   
+// }else{
+//     console.log(num1 , "tek");
+// }
+
+
+//5
+//var num1 = 2
+//let num2 = "2"
+//console.log(num1 == num2);
+//console.log(num1 === num2);
+
+
+//6
+//let num1 = 10
+//console.log(num1 !== 10);
+
+
+
+//7
+// let num1 = prompt("reqem yazin")
+// if(num1 > 5 && num1 < 10){
+//     console.log(num1, "5den boyuk 10dan kicikdir");
+// }else{
+//     console.log(num1, "5den boyuk 10dan kicik deyil");
+// }
+
+
+//8
+// let name1 = null;
+// let name2 = name1 ?? "anonim";
+// console.log(name2);
+
+
+//9
+//let num1 = 2
+// console.log(num1);
+//console.log(!num1);
+
+
+
+//10
+//let num1 = 1
+//let num2 = 2
+//if(num1 && num2){
+//    console.log("true");
+// }else {
+//    console.log("false");
+// }
+
+
+// 11
+// let num1 = 5 < 3
+// let num2 = 3 < 5
+// if(num1 || num2){
+//    console.log("false and true");   
+// }else{
+//    console.log("two false");
+// }
+
+
+//12
+// let num1 = 8;
+// if (num1 > 0 && num1 % 2 == 0) {
+//     console.log("hem musbet hem de cutdur");
+// } else {
+//     console.log("musbet ve ya cut deyil");
+// }
+
+
+//13
+// let user = "user95";
+// let password = "password95";
+
+// let trueUser = "user95";
+// let truePassword = "password95";
+
+// if (user === trueUser && password === truePassword) {
+//     console.log("dogrudur");
+// } else {
+//     console.log("yanlisdir");
+// }
+
+
+//14
+// let num1 = -5;
+// switch (true) {
+//     case (num1 < 0):
+//         console.log("eded menfidir");
+//         break;
+//     case (num1 == 0):
+//         console.log("eded sifirdir");
+// }
